cf8d578deb0540293a02a03d284f7273edd0b035
yemi-protip <yemi.ojedapo@protip.in>
2025-05-31 09:35:27 +0100

    Fix: bind server to port 0.0.0

--- main.py
+++ main.py
@@ -57,8 +57,10 @@ def get_ai_suggestion():
             return f"AI Service failed"
 
 
+
+port = int(os.environ.get("PORT", 5000)) 
 if __name__ == "__main__":
-     app.run(port=5000)
+    app.run(host="0.0.0.0", port=port)
 
         
 
